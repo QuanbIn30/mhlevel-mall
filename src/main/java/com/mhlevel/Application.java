dbf02a4c8a13feccaf2d9ad5710c2c0e73a367cd
QuanbIn30 <821996940@qq.com>
2021-03-18 22:11:51 +0800

    add spring-boo-starter-web and test it

--- src/main/java/com/mhlevel/Application.java
+++ src/main/java/com/mhlevel/Application.java
@@ -1,9 +1,23 @@
 package com.mhlevel;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author quanbin
  * @date 2021-03-02
  */
+@SpringBootApplication
+@RestController
 public class Application {
-    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello!";
+    }
 }
