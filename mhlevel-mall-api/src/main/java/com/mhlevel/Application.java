9d735391bc464c06e1c37e577d9bad798d38c92a
QuanbIn30 <821996940@qq.com>
2021-03-18 23:27:12 +0800

    构建项目基础骨架-引入web模块

---
+++ mhlevel-mall-api/src/main/java/com/mhlevel/Application.java
@@ -0,0 +1,8 @@
+package com.mhlevel;
+
+/**
+ * @author quanbin
+ * @date 2021-03-18
+ */
+public class Application {
+}
---
+++ mhlevel-mall-api/src/main/java/com/mhlevel/controller/Hello.java
@@ -0,0 +1,8 @@
+package com.mhlevel.controller;
+
+/**
+ * @author quanbin
+ * @date 2021-03-18
+ */
+public class Hello {
+}
