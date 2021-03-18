479101755d2fb06a8d647251bfd3f0e8ec06c59b
QuanbIn30 <821996940@qq.com>
2021-03-18 23:28:08 +0800

    构建项目基础骨架-引入web模块

--- mhlevel-mall-api/src/main/java/com/mhlevel/Application.java
+++ mhlevel-mall-api/src/main/java/com/mhlevel/Application.java
@@ -1,8 +1,15 @@
 package com.mhlevel;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author quanbin
  * @date 2021-03-18
  */
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- mhlevel-mall-api/src/main/java/com/mhlevel/controller/Hello.java
+++ mhlevel-mall-api/src/main/java/com/mhlevel/controller/Hello.java
@@ -1,8 +1,17 @@
 package com.mhlevel.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author quanbin
  * @date 2021-03-18
  */
+@RestController
 public class Hello {
+
+    @GetMapping("/hello")
+    public Object hello(){
+        return "Hello MHLEVEL";
+    }
 }
